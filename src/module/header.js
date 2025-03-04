54b537efba07b707c11286588780f3349fb7f00a
Ilya30432 <IlyaS30432@gmail.com>
2025-03-05 00:11:50 +0100

    Adding adaptives

--- src/module/header.js
+++ src/module/header.js
@@ -1,6 +1,7 @@
 const btnOpen = document.querySelector(".burger__open")
 const btnClose = document.querySelector(".burger__close")
 const nav = document.querySelector(".nav")
+const btn = document.querySelector(".head__btn");
 
 
 btnOpen.addEventListener("click",function(){
@@ -9,3 +10,15 @@ btnOpen.addEventListener("click",function(){
 btnClose.addEventListener("click",function(){
     nav.classList.remove("active")
 })
+ function updateButtonText() {
+      if (window.innerWidth >= 1024) {
+        btn.textContent = "Войти";
+      } else{
+        btn.textContent = "Начать беседу"
+      }
+    }
+  // Запускаем при загрузке
+    updateButtonText();
+    // Обновляем при изменении размера экрана
+    window.addEventListener("resize", updateButtonText);
+  
