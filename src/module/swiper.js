048d6fcc3f06f760534e702b3cbe3dbba75d64d0
Ilya30432 <IlyaS30432@gmail.com>
2025-03-30 23:20:50 +0200

    Adding section App and adding section Reviews

--- src/module/swiper.js
+++ src/module/swiper.js
@@ -27,3 +27,28 @@ const swiper = new Swiper('.my-swiper', {
     }
   },
 });
+ 
+// swiper reviews
+
+const swiperReviews = new Swiper('.reviews-swiper', {
+  slidesPerView: 1, 
+  spaceBetween: 10, 
+  freeMode: true, // Включаем свободный режим
+  modules: [Pagination],
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+    }
+  },
+});
